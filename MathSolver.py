adb4495d49e31fadb1fbc2cfd56f047cde9344b0
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2022-11-15 19:20:03 +0000

    Update levels

--- MathSolver.py
+++ MathSolver.py
@@ -2,6 +2,11 @@ import random
 def Rules():
     print("Welcome to another challenge with Ananse.")
     print("Here are the rules:")
+    menu = eval(input("Type 0 to go back to the main menu: "))
+    if menu == 0:
+        main()
+    else:
+        pass
 
 def ScoreBoard():
     pass
@@ -10,70 +15,123 @@ def levelOne():
     name = input("Now, enter your name to begin: ")
     earnable_points = 5
     points = []
-    a = random.randint(0,9)
-    b = random.randint(0,9)
-    signlist = ['+','-','/','*']
-    c = random.randint(0,3)
-    if c == 0:
-        ans = a + b
-    elif c == 1:
-        ans = a - b
-    elif c == 2:
-        ans = a/b
-    else:
-        ans = a*b
-    print("What is ", a, signlist[c],b, "?")
-    user_ans = eval(input("Answer here: "))
-    count = 1
-    while user_ans != ans and count != 3:
-        count += 1
-        earnable_points -= 1
-        print("Please try again")
+    question = 0
+
+    while question < 3:
+        a = random.randint(0,9)
+        b = random.randint(1,9)
+        signlist = ['+','-','/','*']
+        c = random.randint(0,3)
+        if c == 0:
+            ans = a + b
+        elif c == 1:
+            ans = a - b
+        elif c == 2:
+            ans = a//b
+        else:
+            ans = a*b
         print("What is ", a, signlist[c],b, "?")
         user_ans = eval(input("Answer here: "))
-    if count == 0 or count < 3:
-        points.append(earnable_points)
-    else:
-        points.append(1)
-        print("The answer is ", ans)
-    # print(points)
-  
+        count = 1
+        while user_ans != ans and count != 3:
+            count += 1
+            earnable_points -= 1
+            print("Please try again")
+            print("What is ", a, signlist[c],b, "?")
+            user_ans = eval(input("Answer here: "))
+        if count == 0 or count < 3:
+            points.append(earnable_points)
+            print("Well done! You have earned,", earnable_points)
+        else:
+            points.append(1)
+            print("The answer is ", ans)
+        question +=1
+    print("Congratulations! You have earned: ",sum(points), "points.")
 
-
-
-    # print("What is ", a, "+ ",b, "?")
-    # user_ans1 = eval(input("Answer here: "))
-    # if user_ans1 == ans:
-    #     print("Correct for 5 points")
-    #     points.append(5)
-    # elif user_ans1 != ans:
-    #     print("Please try again")
-    #     print("What is ", a, "+ ",b, "?")
-    #     user_ans2 = eval(input("Answer here: "))
-        
-    #     if user_ans2 == ans:
-    #         print("Good! You have earned 3 points")
-    #         points.append(3)
-    #     elif user_ans2 != ans:
-    #         print("Please try again")
-    #         print("What is ", a, "+ ",b, "?")
-    #         user_ans3 = eval(input("Answer here: "))
-    #         if user_ans3 == ans:
-    #             print("You did it. You have earned 2 points")
-    #             points.append(2)
-    #         elif user_ans3 != ans:
-    #             print("Oops. That is wrong but you earn 1 point for trying")
-            
-            
               
-levelOne()        
+# levelOne()        
 
 
 def levelTwo():
-    pass
+    name = input("Now, enter your name to begin: ")
+    earnable_points = 5
+    points = []
+    question = 0
+
+    while question < 3:
+        a = random.randint(0,99)
+        b = random.randint(1,99)
+        signlist = ['+','-','/','*']
+        c = random.randint(0,3)
+        if c == 0:
+            ans = a + b
+        elif c == 1:
+            ans = a - b
+        elif c == 2:
+            ans = a//b
+        else:
+            ans = a*b
+        print("What is ", a, signlist[c],b, "?")
+        user_ans = eval(input("Answer here: "))
+        count = 1
+        while user_ans != ans and count != 3:
+            count += 1
+            earnable_points -= 1
+            print("Please try again")
+            print("What is ", a, signlist[c],b, "?")
+            user_ans = eval(input("Answer here: "))
+        if count == 0 or count < 3:
+            points.append(earnable_points)
+            print("Well done! You have earned,", earnable_points)
+        else:
+            points.append(1)
+            print("The answer is ", ans)
+        question +=1
+    print("Congratulations! You have earned: ",sum(points), "points.")
+
+    
+
+# levelTwo()
 
 def levelThree():
-    pass
+    name = input("Now, enter your name to begin: ")
+    earnable_points = 5
+    points = []
+    question = 0
+
+    while question < 3:
+        a = random.randint(0,99)
+        b = random.randint(1,99)
+        d = random.randint(1,99)
+        signlist = ['+','-']
+        c = random.randint(0,1)
+        e = random.randint(0,1)
+        if c == 0 and e == 0:
+            ans = a + b +d
+        elif c ==  1 and e == 1:
+            ans = a - b -d
+        elif c == 0 and e == 1:
+            ans = a+b-d
+        else:
+            ans = a-b+d
+        print("What is ", a, signlist[c],b,signlist[e],d, "?")
+        user_ans = eval(input("Answer here: "))
+        count = 1
+        while user_ans != ans and count != 3:
+            count += 1
+            earnable_points -= 1
+            print("Please try again")
+            print("What is ", a, signlist[c],b,signlist[e],d, "?")
+            user_ans = eval(input("Answer here: "))
+        if count == 0 or count < 3:
+            points.append(earnable_points)
+            print("Well done! You have earned,", earnable_points)
+        else:
+            points.append(1)
+            print("The answer is ", ans)
+        question +=1
+    print("Congratulations! You have earned: ",sum(points), "points.")
+
 
 # Function to begin the game
 def Game():
@@ -81,7 +139,7 @@ def Game():
     print("Please choose the level.")
     print("Type 1 for Beginner level")
     print("Type 2 for Intermediate level")
-    print("Type 2 for Advanced level")
+    print("Type 3 for Advanced level")
     level = eval(input("Level: "))
     if level == 1:
         levelOne()
@@ -109,7 +167,7 @@ def Begin():
 
     
 
-# def main():
-#     Begin()
-# main()
+def main():
+    Begin()
+main()
     
